a1f6b71d4d8bd70065bb3a33990513d81814e26a
Aaron Hellam <aaronhellam@gmail.com>
2022-07-07 09:15:59 -0700

    week

--- client/src/components/WeekTableHead.js
+++ client/src/components/WeekTableHead.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const WeekTableHead = ({ shifts }) => {
   return (
     <thead className="text-center">
-          <tr className="bg-slate-300">
-            {[" ", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map(
-              (col, index) => {
-                let totalHours = 0
-                for (const employee of shifts) {
-                  const shift = employee.shifts.find(shift => shift.day === index - 1)
-                  totalHours += shift ? shift.duration : 0
-                }
-                return (<td key={index} className="font-bold border-[1px] border-black">{col} {index > 0 && `(${totalHours} hrs)`}</td>)
-              }
-            )}
-          </tr>
+      <tr className="bg-slate-300">
+        {[" ", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map(
+          (weekDayCol, index) => {
+            let totalHours = 0;
+            for (const employee of shifts) {
+              const shift = employee.shifts.find((shift) => shift.day === index - 1);
+              totalHours += shift ? shift.duration : 0;
+            }
+            return (
+              <td key={index} className="font-bold border-[1px] border-black">
+                {weekDayCol} {index > 0 && `(${totalHours} hrs)`}
+              </td>
+            );
+          }
+        )}
+      </tr>
     </thead>
-  )
-}
+  );
+};
 
-export default WeekTableHead
+export default WeekTableHead;
